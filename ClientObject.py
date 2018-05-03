bf2610bacefaf751297ec869b188f7f313b6c9a0
Jussi Jokio <jujokio@student.oulu.fi>
2018-05-03 17:11:52 +0300

    init0

---
+++ ClientObject.py
@@ -0,0 +1,34 @@
+import socket
+
+
+class ClientObject:
+
+	def SendJoinRequest:
+		print("Joining...")
+		requestCode = 0
+		return None
+		
+		
+	def SendStandRequest:
+		print("Standing...")
+		requestCode = 1
+		return None
+
+	def SendHitRequest:
+		print("Hitting...")
+		requestCode = 2
+		return None	
+		
+		
+	def SendRageQuitRequest:
+		print("Quiting...")
+		requestCode = -1
+		return None
+		
+		
+		
+	def SendRequest:
+		print("requesting...")
+		return None
+		
+		
